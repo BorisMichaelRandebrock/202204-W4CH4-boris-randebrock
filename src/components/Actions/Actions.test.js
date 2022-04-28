c5c53c8ea6bc238af5262d633b7524bb211698ca
Boris <boris@randebrock.com>
2022-04-28 22:57:30 +0200

    added two and a half tests

---
+++ src/components/Actions/Actions.test.js
@@ -0,0 +1,8 @@
+// import { render } from "@testing-library/react";
+// import Actions from "./Actions";
+
+describe("Given the Actions Component", () => {
+  describe("When", () => {
+    test("Then", () => {});
+  });
+});
